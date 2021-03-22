70ff07583cdc4a4a80d038a63ffffc922f1ee748
Paulyash9 <paulinio09@gmail.com>
2021-03-23 01:54:01 +0300

    added settings

---
+++ dags/settings.py
@@ -0,0 +1,11 @@
+# Create app in Spotify (https://developer.spotify.com/dashboard/applications) and get id and secret from there
+spotipy_client_id = ''
+spotipy_client_secret = ''
+
+# Postgresql connection info
+db_user = ''
+db_password = ''
+db_server = ''
+port = '5432'
+db = ''
+schema = ''
